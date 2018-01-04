508468ffb5c0694a7a670b8969c17ad89eedd18c
jarhill0 <jarhill0@users.noreply.github.com>
2018-01-04 11:18:15 -0800

    🔧 Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+VERSION = '0.0.1a0'
+
+setup(name='hnpy',
+      author='jarhill0',
+      author_email='',
+      description='Yet another object-based Hacker News API wrapper for Python.',
+      install_requires=['requests >= 2.18.4'],
+      keywords='hacker news api wrapper python3',
+      license='MIT',
+      long_description='Click here for README: https://github.com/jarhill0/hnpy#hnpy',
+      packages=['hnpy'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      test_suite='tests',
+      url='https://github.com/jarhill0/hnpy',
+      version=VERSION)
